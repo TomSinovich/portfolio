0021ca75ed054e6eb0f012b551b7130156ff73de
tom.sinovich@gmail.com <tomsinovich@gmail.com>
2019-11-11 10:34:15 -0600

    adds download link and logo; adds gatsbyJS project

--- src/components/info.js
+++ src/components/info.js
@@ -3,7 +3,8 @@ import {
   FaGithub as Github,
   FaInstagram as Insta,
   FaEnvelope as Mail,
-  FaLinkedin as LinkedIn
+  FaLinkedin as LinkedIn,
+  FaFileDownload as FileDownload,
 } from "react-icons/fa"
 
 export default () => {
@@ -13,39 +14,66 @@ export default () => {
       <div className="display-4" style={{ color: "#9E66CC" }}>
         <span style={{ color: "#324359" }}>Hey, I'm</span> <br />
         Tom
-        <br/> Sinovich.
+        <br /> Sinovich.
       </div>
       <div className="h1 code mt-4 mb-3">{"<>"}</div>
       <div className="text-muted mx-5 my-4 h3 info">
         Savvy Full Stack Engineer
         <br />
         <div className="hide-mobile">
-        and
-        <br />
-        <div className="slider">
-          <div className="caption">
-            <div className="text-box">
-              <div>Advocate for Social Justice </div>
-              <div>Cat Dad</div>
-              <div>Kombucha Brewer</div>
+          and
+          <br />
+          <div className="slider">
+            <div className="caption">
+              <div className="text-box">
+                <div>Advocate for Social Justice </div>
+                <div>Cat Dad</div>
+                <div>Kombucha Brewer</div>
+              </div>
             </div>
           </div>
-        </div>
         </div>
       </div>
       <br />
       <div className="h1 code mt-2 mb-3">{"</>"}</div>
       <div className="h1 mt-5 icon-wrapper">
-        <a className="mr-5 icon" href="https://github.com/tomsinovich" target="_blank" rel="noopener noreferrer">
+        <a
+          className="mr-5 icon"
+          href="../../content/Tom_Sinovich_Resume.pdf"
+          download
+        >
+          <FileDownload />
+        </a>
+        <a
+          className="mr-5 icon"
+          href="https://github.com/tomsinovich"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <Github />
         </a>
-        <a className="mr-5 icon" href="https://www.instagram.com/tomspinach/" target="_blank" rel="noopener noreferrer">
+        <a
+          className="mr-5 icon"
+          href="https://www.instagram.com/tomspinach/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <Insta />
         </a>
-        <a className="mr-5 icon" href="mailto:tomsinovich@gmail.com" target="_blank" rel="noopener noreferrer">
+        <a
+          className="mr-5 icon"
+          href="mailto:tomsinovich@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <Mail />
         </a>
-        <a className="mr-5 icon" href="https://www.linkedin.com/in/tom-sinovich-25b4a3112/" target="_blank" rel="noopener noreferrer">
+        <a
+          className="mr-5 icon"
+          href="https://www.linkedin.com/in/tom-sinovich-25b4a3112/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <LinkedIn />
         </a>
       </div>
